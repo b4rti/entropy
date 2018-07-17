b52621f220fe6544b54b87fae3a3febebf31b145
b4rti <b.gorol@efero.de>
2018-07-18 01:50:52 +0200

    Network

---
+++ cluster/network.go
@@ -0,0 +1,10 @@
+package ecluster
+
+func CheckNetwork(n string) bool {
+
+	return false
+}
+
+func CreateNetwork(n string) {
+	
+}
--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/b4rti/entropy/cluster"
 
 	"github.com/b4rti/entropy/plugin"
 )
 
 func main() {
+	c := &Config{}
+
+	if _, err := os.Stat("/etc/entropy/config.yml"); os.IsNotExist(err) {
+		c = CreateDefaultConfig()
+	} else {
+		c = LoadConfig()
+	}
+
+	if !ecluster.CheckNetwork(c.NetworkName) {
+		ecluster.CreateNetwork(c.NetworkName)
+	}
+
 	go eplugin.NewEntropyPlugin().Serve()
 
 	fmt.Scanln()
-
 }
